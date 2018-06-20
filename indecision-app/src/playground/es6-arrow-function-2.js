ba45932a22c00cd54cfb9e793366488cdb5d9798
Duorgyalp <duorgyalp.ymranootgnok@gmail.com>
2018-06-20 07:49:46 +0700

    ES6 Aside: Arrow Functions Part II

--- indecision-app/src/playground/es6-arrow-function-2.js
+++ indecision-app/src/playground/es6-arrow-function-2.js
@@ -1,24 +1,18 @@
-'use strict';
-
 // arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
   //console.log(arguments);
   return a * b;
-};
+}
 console.log(add(55, 1, 20));
 
 // this keyword - no longer bound
 
-var user = {
+const user = {
   name: 'Alan',
   cities: ['Philadelphia', 'New York', 'Dublin'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + 'has lived in ' + city;
-    });
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + 'has lived in ' + city);
 
     // this.cities.forEach((city) => {
     //   console.log(this.name + 'has lived in ' + city);
@@ -29,15 +23,11 @@ console.log(user.printPlacesLived());
 
 // Chanllenge area
 
-var multipier = {
+const multipier = {
   number: [10, 20, 30],
   multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.number.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
+  multiply() {
+    return this.number.map((number) => number * this.multiplyBy);
   }
 };
 
